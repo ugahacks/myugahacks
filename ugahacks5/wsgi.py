0d25a90b14d3ccd0444da870c05b357bd4d1e553
Noah Barnette <noahbarnette@gmail.com>
2019-07-01 18:32:38 -0400

    added seperate settings for local and prod

--- ugahacks5/wsgi.py
+++ ugahacks5/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from settings.production import * as settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ugahacks5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
 
 application = get_wsgi_application()
