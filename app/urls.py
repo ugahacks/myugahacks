d6eba37b6b89949162b176dab96cfbf2f53b49d4
Noah Barnette <noahbarnette@gmail.com>
2019-07-17 11:58:05 -0400

    Added new homepage at /test

--- app/urls.py
+++ app/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     url(r'^files/(?P<file_>.*)$', views.protectedMedia, name="protect_media"),
     url(r'^meals/', include('meals.urls')),
     url(r'^judging/', include('judging.urls')),
+    url(r'test/$', views.home5, name='UGAHacks 5'),
 ]
 
 if settings.BAGGAGE_ENABLED:
