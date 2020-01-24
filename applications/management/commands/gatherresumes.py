2a5c3b5c5321d366b066053f0872bb0c89ded90a
Julian Camp <38470746+jcamp5@users.noreply.github.com>
2020-01-24 15:00:40 -0500

    Built the export resume function

---
+++ applications/management/commands/gatherresumes.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from applications.models import Application
+import os
+import tarfile
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print("Exporting resumes: This may take a few minutes...")
+
+        users = Application.objects.filter(status="C") | Application.objects.filter(status="A")
+        
+        with tarfile.open(f"./files/resumes/resume_export.tar.gz", "w:gz") as tar_handle:
+            for resume in users:
+                tar_handle.add(f"./{resume.resume.url}", resume.resume.name)
+        
+        print("Finished exporting resumes.")
