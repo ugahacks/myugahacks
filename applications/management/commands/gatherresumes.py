a80b624de52bfe3fc27f17134e80d0cab9d6cfc9
Julian Camp <38470746+jcamp5@users.noreply.github.com>
2020-01-24 20:41:23 -0500

    Added error handling to gatherresumes command to fix the case where the user didn't have a resume.

--- applications/management/commands/gatherresumes.py
+++ applications/management/commands/gatherresumes.py
@@ -9,11 +9,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Gathering Resumes: This may take a few minutes...")
         try:
-            users = Application.objects.filter(status="C") | Application.objects.filter(status="A")
-            
+            users = (Application.objects.filter(status="C") | Application.objects.filter(status="A"))
+        
             with tarfile.open(f"./files/resumes/resume_export.tar.gz", "w:gz") as tar_handle:
                 for resume in users:
-                    tar_handle.add(f"./{resume.resume.url}", resume.resume.name)
+                    if resume.resume:
+                        tar_handle.add(f"./{resume.resume.url}", resume.resume.name)
             print("Finished gathering resumes.")
         except:
             print("Error: gathering resumes.")
