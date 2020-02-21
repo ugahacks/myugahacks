6cfaebc464d12849f9f24cd40bc8ca6ca97df416
Andrei Spatariu <andros415@gmail.com>
2020-02-20 22:41:43 -0500

    Updated Hackathon vars to include links to social media

--- app/hackathon_variables.py
+++ app/hackathon_variables.py
@@ -22,13 +22,13 @@ HACKATHON_OG_IMAGE = 'https://cdn.discordapp.com/attachments/620786563382640650/
 # (OPTIONAL) Track visits on your website
 HACKATHON_GOOGLE_ANALYTICS = ''
 # (OPTIONAL) Hackathon Twitter user
-HACKATHON_TWITTER_ACCOUNT = ''
+HACKATHON_TWITTER_ACCOUNT = 'https://twitter.com/ugahacks'
 # (OPTIONAL) Hackathon Facebook page
-HACKATHON_FACEBOOK_PAGE = ''
+HACKATHON_FACEBOOK_PAGE = 'https://www.facebook.com/ugahacks/'
 # (OPTIONAL) Hackathon YouTube channel
 HACKATHON_YOUTUBE_PAGE = ''
 # (OPTIONAL) Hackathon Instagram user
-HACKATHON_INSTAGRAM_ACCOUNT = ''
+HACKATHON_INSTAGRAM_ACCOUNT = 'https://www.instagram.com/ugahacks/'
 # (OPTIONAL) Hackathon Medium user
 HACKATHON_MEDIUM_ACCOUNT = ''
 # (OPTIONAL) Github Repo for this project (so meta)
@@ -50,7 +50,7 @@ HACKATHON_LIVE_PAGE = 'https://my.ugahacks.com/'
 REGEX_HACKATHON_ORGANIZER_EMAIL = '^.*@ugahacks\.com$'
 
 # (OPTIONAL) Send 500 errors to email while on production mode
-HACKATHON_DEV_EMAILS = ['noahbarnette@gmail.com', 'dev-team@ugahacks.com']
+HACKATHON_DEV_EMAILS = ['dev-team@ugahacks.com']
 
 # Baggage configuration
 BAGGAGE_ENABLED = False
