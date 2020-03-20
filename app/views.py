50c564428ddf2e249dda12c97f5a70be39ad3e6a
Andrei Spatariu <andros415@gmail.com>
2020-03-19 22:58:59 -0400

    Edit app view

--- app/views.py
+++ app/views.py
@@ -16,10 +16,8 @@ from app import utils, mixins
 
 def root_view(request):
 
-    return render(request, 'ugahacks6/UGAHacks6-teaser.html')
-    
     if not request.user.is_authenticated:
-        return render(request, 'home-hacks-5.html')
+        return render(request, 'ugahacks6/UGAHacks6-teaser.html')
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('account_login'))
     if not request.user.has_usable_password():
