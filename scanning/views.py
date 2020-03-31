673ee00519ff5bb53f7fee7ca8e27a06829ec0c1
Kane Du <kanedu@pop-os.localdomain>
2020-03-30 23:18:00 -0400

    Fixed error in view

--- scanning/views.py
+++ scanning/views.py
@@ -21,7 +21,7 @@ class ScanningView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(ScanningView, self).get_context_data(**kwargs)
         workshops = Workshop.objects.all()
-        meals = Meals.objects.all()
+        meals = Meal.objects.all()
         context.update({
             'workshops': workshops,
             'meals': meals,
