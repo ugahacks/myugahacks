7e565348cd1bee989b447559247cbbf240c030bb
Andrei Spatariu <andros415@gmail.com>
2020-04-21 20:31:40 -0400

    Added styling to Create Blog Post form and Blog to top navbar

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from froala_editor.widgets import FroalaEditor
 
 class BlogAddForm(forms.ModelForm):
     title = forms.CharField(max_length=256)
-    description = forms.CharField(max_length=256)
+    description = forms.CharField(max_length=256, help_text='Briefly describe the content of your blog post (This will appear when looking at the list of blog posts).')
     tags = forms.CharField(help_text='Enter tags seperated by commas.')
     thumbnail = forms.ImageField()
     content = forms.CharField(widget=FroalaEditor)
