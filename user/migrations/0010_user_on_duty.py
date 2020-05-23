c67eee3876cfcefd95312c472ab2e82b9b533aa3
Shawn Holman <the.ikick@gmail.com>
2020-05-23 18:01:51 -0400

    Fixed migration

--- user/migrations/0010_user_on_duty.py
+++ user/migrations/0010_user_on_duty.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0009_auto_20200416_1517'),
+        ('user', '0008_user_is_hx'),
     ]
 
     operations = [
