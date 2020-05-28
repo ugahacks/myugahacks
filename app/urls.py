eb97c890e12b700b91dccaaf36f629368e2d707a
Andrei Spatariu <andros415@gmail.com>
2020-05-28 18:34:26 -0400

    Remove double import

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,6 @@ from django.urls import include
 from django.urls import path
 from django.views.generic import RedirectView
 
-from django.urls import path
-
 from app import views
 
 urlpatterns = [
