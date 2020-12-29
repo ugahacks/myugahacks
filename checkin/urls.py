581774f8ba259c7050927750ef8a0d0e8f7dbbfd
Neal Patel <nealpatel2021@gamil.com>
2020-12-29 15:06:27 -0500

    refactor

--- checkin/urls.py
+++ checkin/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     url(r'^ranking/$', views.CheckinRankingView.as_view(), name='check_in_ranking'),
     url(r'^reissue/$', views.ReIssueList.as_view(), name='re_issue_list'),
     url(r'^reissue/(?P<id>[\w-]+)$', views.ReIssueHackerView.as_view(), name='re_issue_hacker'),
+    # Legacy non-COVID checkin:
     # url(r'(?P<id>[\w-]+)$', views.CheckInHackerView.as_view(), name='check_in_hacker'),
 
-    # added to keep legacy use of regex pre-refactor
-    # url(r'me/(?P<id>[\w-]+)$', views.OnlineCheckInHackerView.as_view(), name='online_check_in'),
+    # COVID Online checkin:
     path('me/<uuid:id>', views.OnlineCheckInHackerView.as_view(), name='check_in_hacker'),
 ]
