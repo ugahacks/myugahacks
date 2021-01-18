607e8d1490b70079bcfa14f89c27488fc8c28e3f
Andrei Spatariu <andros415@gmail.com>
2021-01-18 15:05:29 -0500

    Make sponsors and role admin more descriptive

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from sponsors import models
 
 # Register your models here.
-admin.site.register(models.SponsorApplication)
-admin.site.register(models.Sponsor)
+
+class SponsorApplicationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tshirt_size', 'diet')
+    search_fields = ('user__email',)
+
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ('company', 'email_domain', 'tier')
+    search_fields = ('company',)
+
+admin.site.register(models.SponsorApplication, admin_class=SponsorApplicationAdmin)
+admin.site.register(models.Sponsor, admin_class=SponsorAdmin)
